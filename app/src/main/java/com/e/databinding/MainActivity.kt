f012c4ac652a82745cfcf86f6c58ec8cb5c22e87
Bismah Qadri <bismahqadri@gmail.com>
2019-12-25 18:44:51 +0530

    click listener handled in data binding

--- app/src/main/java/com/e/databinding/MainActivity.kt
+++ app/src/main/java/com/e/databinding/MainActivity.kt
@@ -3,16 +3,23 @@ package com.e.databinding
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onClick(p0: View?) {
+
+        Toast.makeText(this, "View Clicked",Toast.LENGTH_SHORT).show()
+
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler_view.layoutManager = LinearLayoutManager(this)
-        recycler_view.adapter = MainAdapter(userList())
+        recycler_view.adapter = MainAdapter(userList(), this)
 
 
     }
--- app/src/main/java/com/e/databinding/MainAdapter.kt
+++ app/src/main/java/com/e/databinding/MainAdapter.kt
@@ -5,10 +5,10 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import com.e.databinding.databinding.ItemMainBinding
 
-class MainAdapter(private val userList : List<User>) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
+class MainAdapter(private val userList : List<User>,
+                  private val listener : View.OnClickListener) : RecyclerView.Adapter<MainAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ViewHolder {
         val binding : ItemMainBinding = DataBindingUtil.inflate(
@@ -23,13 +23,14 @@ class MainAdapter(private val userList : List<User>) : RecyclerView.Adapter<Main
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-       holder.bindTo(userList[position])
+       holder.bindTo(userList[position], listener)
     }
 
 
     class ViewHolder(private val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root) {
-        fun bindTo(user: User) {
+        fun bindTo(user: User, listener: View.OnClickListener) {
             binding.userModel = user
+            binding.listener = listener
         }
 
     }
